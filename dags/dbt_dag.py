f5c4d44480131ffa9a66a538af904507531852fb
Chris Crocker <chris.crocker7227@gmail.com>
2024-11-03 21:24:34 -0500

    update filepath

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -15,7 +15,7 @@ profile_config = ProfileConfig(
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("C:\Users\chris\dbt-dag\dags\dbt\data_pipeline",),
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
